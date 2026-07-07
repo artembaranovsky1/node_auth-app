23effe7f4cd44d2d75953618536666d23796c007
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2026-07-07 12:54:43 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import { authRouter } from './routes/auth.route.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3005;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_HOST,
+    credentials: true,
+  }),
+);
+
 app.use(authRouter);
 
 app.get('/', (req, res) => {
