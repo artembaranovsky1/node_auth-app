1b3f96bb5b021cc92aab2e586af250aae1c5b4b5
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2026-07-07 14:40:13 +0300

    send on activate list

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,15 @@
 import { User } from '../models/user.js';
+import { emailService } from '../services/email.service.js';
+import { v4 as uuidv4 } from 'uuid';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   const newUser = await User.create({ name, email, password });
+
+  const token = uuidv4();
+
+  await emailService.sendActivationEmail(email, token);
 
   res.send(newUser);
 };
