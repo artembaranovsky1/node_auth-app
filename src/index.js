1366edd9745091ab283377fae025d2f30d720f11
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2026-07-07 12:48:48 +0300

    conect to db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-'use strict';
+import express from 'express';
+import { authRouter } from './routes/auth.route.js';
+
+const PORT = process.env.PORT || 3005;
+
+const app = express();
+
+app.use(express.json());
+app.use(authRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  // console.log(`Server started on port ${PORT}`);
+});
